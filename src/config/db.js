4dd0fc1bfc079208dca5bb49fa5bb68688a137f2
mario <mariocasanovaramirez@gmail.com>
2022-01-10 00:17:18 -0300

    agregada la env de mongo test

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ require('dotenv').config({ path: ".env" })
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {})
+    // await mongoose.connect(process.env.MONGO_URL_TEST, {})
     console.log("DB conected")
   } catch (error) {
     console.log("Se ha generado un error al conectar con la DB")
