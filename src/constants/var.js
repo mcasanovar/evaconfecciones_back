4a0c335e33477b7cfce5ff1348c17fa90f9a1c56
mario <mariocasanovaramirez@gmail.com>
2022-01-10 00:17:50 -0300

    agregadas las variables para insercion masiva de items

---
+++ src/constants/var.js
@@ -0,0 +1,206 @@
+export const PRICES_BY_SIZE_BUZO = {
+  2: 19900,
+  4: 19900,
+  6: 19900,
+  8: 19900,
+  10: 21900,
+  12: 21900,
+  14: 22900,
+  16: 22900,
+  "S": 23900,
+  "M": 23900,
+  "L": 25900,
+  "XL": 25900,
+  "ESPECIAL (XXL)": 27000
+}
+
+export const PRICES_BY_SIZE_POLERA_GIMNACIA = {
+  2: 7900,
+  4: 7900,
+  6: 7900,
+  8: 7900,
+  10: 8900,
+  12: 8900,
+  14: 8900,
+  16: 9500,
+  "S": 9500,
+  "M": 9900,
+  "L": 9900,
+  "XL": 10900,
+  "ESPECIAL (XXL)": 12000
+}
+
+export const PRICES_BY_SIZE_POLERA_PIQUE = {
+  2: 8900,
+  4: 8900,
+  6: 8900,
+  8: 8900,
+  10: 9500,
+  12: 9500,
+  14: 9500,
+  16: 9900,
+  "S": 9900,
+  "M": 10900,
+  "L": 10900,
+  "XL": 11900,
+  "ESPECIAL (XXL)": 12900
+}
+
+export const PRICES_BY_SIZE_FALDA = {
+  2: 12700,
+  4: 12700,
+  6: 12700,
+  8: 12700,
+  10: 13700,
+  12: 13700,
+  14: 14700,
+  16: 14700,
+  "S": 15700,
+  "M": 15700,
+  "L": 16700,
+  "XL": 16700,
+  "ESPECIAL (XXL)": 18000
+}
+
+export const PRICES_BY_SIZE_CALZA = {
+  2: 7900,
+  4: 7900,
+  6: 7900,
+  8: 7900,
+  10: 8900,
+  12: 8900,
+  14: 8900,
+  16: 9900,
+  "S": 9900,
+  "M": 10900,
+  "L": 10900,
+  "XL": 11500,
+  "ESPECIAL (XXL)": 12500
+}
+
+export const PRICES_BY_SIZE_SHORT = {
+  2: 7500,
+  4: 7500,
+  6: 7500,
+  8: 7500,
+  10: 8500,
+  12: 8500,
+  14: 9500,
+  16: 9500,
+  "S": 10500,
+  "M": 10500,
+  "L": 11500,
+  "XL": 11500,
+  "ESPECIAL (XXL)": 12500
+}
+
+export const PRICES_BY_SIZE_PANTALON_BUZO = {
+  2: 8500,
+  4: 8500,
+  6: 8500,
+  8: 8500,
+  10: 9500,
+  12: 9500,
+  14: 10500,
+  16: 10500,
+  "S": 11500,
+  "M": 11500,
+  "L": 12500,
+  "XL": 12500,
+  "ESPECIAL (XXL)": 13500
+}
+
+export const PRICES_BY_SIZE_JERSEY = {
+  2: 12500,
+  4: 12500,
+  6: 12500,
+  8: 12500,
+  10: 13500,
+  12: 13500,
+  14: 15900,
+  16: 15900,
+  "S": 16900,
+  "M": 16900,
+  "L": 18000,
+  "XL": 18000,
+  "ESPECIAL (XXL)": 20000
+}
+
+export const PRICES_BY_SIZE_CHAQUETA_POLAR = {
+  2: 12900,
+  4: 12900,
+  6: 12900,
+  8: 12900,
+  10: 14900,
+  12: 14900,
+  14: 15900,
+  16: 15900,
+  "S": 16900,
+  "M": 16900,
+  "L": 17900,
+  "XL": 17900,
+  "ESPECIAL (XXL)": 19000
+}
+
+export const PRICES_BY_SIZE_CHAQUETA_BUZO = {
+  2: 11900,
+  4: 11900,
+  6: 11900,
+  8: 11900,
+  10: 12900,
+  12: 12900,
+  14: 13900,
+  16: 13900,
+  "S": 14900,
+  "M": 14900,
+  "L": 15900,
+  "XL": 15900,
+  "ESPECIAL (XXL)": 16900
+}
+
+export const PRICES_BY_SIZE_PG_MANGA_LARGA = {
+  2: 8900,
+  4: 8900,
+  6: 8900,
+  8: 8900,
+  10: 9900,
+  12: 9900,
+  14: 9900,
+  16: 10500,
+  "S": 10500,
+  "M": 10900,
+  "L": 10900,
+  "XL": 11900,
+  "ESPECIAL (XXL)": 13000
+}
+
+export const PRICES_BY_SIZE_PP_MANGA_LARGA = {
+  2: 9900,
+  4: 9900,
+  6: 9900,
+  8: 9900,
+  10: 10500,
+  12: 10500,
+  14: 10500,
+  16: 10900,
+  "S": 10900,
+  "M": 11900,
+  "L": 11900,
+  "XL": 12900,
+  "ESPECIAL (XXL)": 13900
+}
+
+export const SELECTION_PRICES = {
+  "Buzo": PRICES_BY_SIZE_BUZO,
+  "Polera Gimnasia": PRICES_BY_SIZE_POLERA_GIMNACIA,
+  "Polera Piqué": PRICES_BY_SIZE_POLERA_PIQUE,
+  "Falda": PRICES_BY_SIZE_FALDA,
+  "Calza": PRICES_BY_SIZE_CALZA,
+  "Short": PRICES_BY_SIZE_SHORT,
+  "Pantalón Buzo": PRICES_BY_SIZE_PANTALON_BUZO,
+  "Jersey": PRICES_BY_SIZE_JERSEY,
+  "Chaqueta Polar": PRICES_BY_SIZE_CHAQUETA_POLAR,
+  "Chaqueta Buzo": PRICES_BY_SIZE_CHAQUETA_BUZO,
+  "Polera Gimnasia Manga Larga": PRICES_BY_SIZE_PG_MANGA_LARGA,
+  "Polera Piqué Manga Larga": PRICES_BY_SIZE_PP_MANGA_LARGA
+}
