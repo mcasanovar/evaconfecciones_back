95a2d80e846c277e83ec89671a30c6a042528eef
mario <mariocasanovaramirez@gmail.com>
2022-01-11 00:05:22 -0300

    verificar si la url de mongo es local o produccion

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,8 @@ require('dotenv').config({ path: ".env" })
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {})
-    // await mongoose.connect(process.env.MONGO_URL_TEST, {})
+    // await mongoose.connect(process.env.MONGO_URL, {})
+    await mongoose.connect(process.env.MONGO_URL_TEST || process.env.MONGO_URL, {})
     console.log("DB conected")
   } catch (error) {
     console.log("Se ha generado un error al conectar con la DB")
