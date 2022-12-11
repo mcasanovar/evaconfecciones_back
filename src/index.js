1d1c475dcee3687d9738332ea538aaaf779367af
Mario <mcasanova@tactech.cl>
2022-12-11 15:13:08 -0300

    agregado cors y async jwt

--- src/index.js
+++ src/index.js
@@ -1,41 +1,46 @@
-import { ApolloServer } from 'apollo-server'
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
-import typeDefs from './types/index.js'
-import resolvers from './resolvers/index.js'
-import connectDb from './config/db.js'
-import { JsonWebToken } from './libs/index.js'
+import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import typeDefs from "./types/index.js";
+import resolvers from "./resolvers/index.js";
+import connectDb from "./config/db.js";
+import { JsonWebToken } from "./libs/index.js";
 
-require('dotenv').config({ path: ".env" })
+require("dotenv").config({ path: ".env" });
 
 //libs
-const { verifyToken } = JsonWebToken
+const { verifyToken } = JsonWebToken;
 
 //db
-connectDb()
+connectDb();
 
 //server
 const server = new ApolloServer({
+  cors: {
+    origin: [
+      "https://evaconfecciones-front.vercel.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  },
   typeDefs,
   resolvers,
   context: ({ req }) => {
-    const token = req.headers['authorization'] || ''
-    if(token){
+    const token = req.headers["authorization"] || "";
+    if (token) {
       try {
         const userLogged = verifyToken(token, process.env.SECRET_JWT);
         return {
-          userLogged
-        }
+          userLogged,
+        };
       } catch (error) {
-        console.log("Hubo un error al encontrar el usuario logeado")
-        console.log(error)
+        console.log("Hubo un error al encontrar el usuario logeado");
+        console.log(error);
       }
     }
   },
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground()
-  ]
-})
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`Server running on ${url}`)
-})
+  console.log(`Server running on ${url}`);
+});
--- src/libs/jsonwebtoken.js
+++ src/libs/jsonwebtoken.js
@@ -1,15 +1,15 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 const createToken = (user, secret, expiresIn) => {
-  const { _id, firstName, lastName, email } = user
-  return jwt.sign({ _id, firstName, lastName, email }, secret, { expiresIn })
-}
+  const { _id, firstName, lastName, email } = user;
+  return jwt.sign({ _id, firstName, lastName, email }, secret, { expiresIn });
+};
 
-const verifyToken = (token, secret) => {
-  return jwt.verify(token, secret)
-}
+const verifyToken = async (token, secret) => {
+  return await jwt.verify(token, secret);
+};
 
-export default { 
+export default {
   createToken,
-  verifyToken
-}
+  verifyToken,
+};
